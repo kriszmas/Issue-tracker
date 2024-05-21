96026521b88b3c0b76d27d9b9d6c6cf4fc068c97
Krisztian Danko <krisz.danko05@gmail.com>
2024-05-21 18:40:50 +0200

    Loading Skeleton hozzáadása

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Skeleton } from "@/app/components";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
@@ -63,12 +64,14 @@ const NavLinks = () => {
 const AuthStatus = () => {
   const { status, data: session } = useSession();
 
-  if (status === "loading") return null;
+  if (status === "loading") return <Skeleton width="3rem" />;
 
-  if (status === "unauthenticated") return;
-  <Link className="nav-link" href="/api/auth/signin">
-    Login
-  </Link>;
+  if (status === "unauthenticated")
+    return (
+      <Link className="nav-link" href="/api/auth/signin">
+        Login
+      </Link>
+    );
 
   return (
     <Box>
